cf56d63b0c3be861e2fd3c2cf0aed81468fdfff5
Declan Murphy <declxn@gmail.com>
2021-04-14 18:15:36 -0500

    Added useFrame method to the cubeCamera, which finally allowed for the reflective/metal material to reflect the outer image.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Canvas, useThree } from 'react-three-fiber'
+import { Canvas, useThree, useFrame } from 'react-three-fiber'
 import { CubeTextureLoader, LinearMipmapLinearFilter, WebGLCubeRenderTarget, RGBFormat, CubeCamera } from 'three'
 import "./styles.css";
 
@@ -29,11 +29,15 @@ function Sphere() {
   const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
   cubeCamera.position.set(0, 0, 0);
   scene.add(cubeCamera);
+
+  useFrame(() => cubeCamera.update(gl, scene))
+
   return (
     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
       <sphereGeometry attach="geometry" args={[2, 32, 32]} />
       <meshBasicMaterial
         attach="material"
+        envMap={cubeCamera.renderTarget.texture}
         color="white"
         roughness={0.1}
         metalness={1}
@@ -45,6 +49,7 @@ function Sphere() {
 export default function App() {
   return (
     <Canvas>
+      {/* <CameraControls /> */}
       <Sphere />
       <Sandbox />
     </Canvas>
