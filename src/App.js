fdc0f0005d8cd661615360c85c54150619a14172
Declan Murphy <declxn@gmail.com>
2021-04-14 17:47:32 -0500

    Got the sphere up on tn the canvas.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { Canvas } from 'react-three-fiber'
+import { Canvas, useThree } from 'react-three-fiber'
+import { CubeTextureLoader } from 'three'
 import "./styles.css";
+
+function Sandbox() {
+  const { scene } = useThree()
+  const loader = new CubeTextureLoader();
+  const texture
+}
+
+function Sphere() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  )
+}
 
 export default function App() {
   return (
-    <Canvas></Canvas>
+    <Canvas>
+      <Sphere />
+    </Canvas>
   );
 }
